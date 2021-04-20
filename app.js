18e8d77c102cf1f90751cd08535541c70a5f9768
Ian <iankjera@gmail.com>
2021-04-19 21:51:38 -0400

    used css grid to create calculator layout. added event listener to numbers. basic styling. basic math functions added.

--- app.js
+++ app.js
@@ -1,1 +1,59 @@
-console.log('test')
+const container = document.querySelector('.main-container');
+const numbers = document.querySelectorAll('.number');
+
+let selectedNumber;
+
+numbers.forEach(number => {
+    number.addEventListener('click', e => {
+        selectedNumber = e.target.innerText;
+        console.log(selectedNumber)
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//basic math functions
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+
+
+// function createButtonGrid(num) {
+//     let i = 0;
+//     while (i < num) {
+//         const gridElement = document.createElement('div');
+//         gridElement.classList.add('button');
+//         container.append(gridElement);
+//         i++
+//     }
+// }
+
