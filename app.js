1c405a6dc4420140326b49f5712e60c0582b61ca
Ian <iankjera@gmail.com>
2021-04-21 21:13:20 -0400

    barely works at this point. using an array to store numbers to perform calculations on

--- app.js
+++ app.js
@@ -1,59 +1,91 @@
 const container = document.querySelector('.main-container');
 const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator')
+const addition = document.querySelector('#add');
+const subtraction = document.querySelector('#subtract');
+const division = document.querySelector('#divide');
+const multiplication = document.querySelector('#multiply');
+const equals = document.querySelector('#equals');
 
+let operator = '';
 let selectedNumber;
+let operationRunning = false;
+let adding = false;
+let subtracting = false;
+let dividing = false;
+let multiplying = false;
+
+let nums2Calc = []
+
+let calculations = [
+    {
+        name: 'add',
+        operating: false,
+        calc: add
+    },
+    {
+        name: 'subtract',
+        operating: false,
+        calc: subtract
+    },
+    {
+        name: 'multiply',
+        operating: false,
+        calc: multiply
+    },
+    {
+        name: 'divide',
+        operating: false,
+        calc: divide
+    }
+];
 
 numbers.forEach(number => {
     number.addEventListener('click', e => {
-        selectedNumber = e.target.innerText;
-        console.log(selectedNumber)
+        nums2Calc.push(parseInt(e.target.innerText))
+        console.log(nums2Calc);
+    })
+
+})
+
+operators.forEach(operator => {
+    operator.addEventListener('click', e => {
+        operationRunning = true;
+        calculations.forEach(calc => {
+            if (calc.name === e.target.id) {
+                calc.operating = true
+                // operator = e.target.id
+                // console.log(operator)
+            }
+        })
+
     })
 })
 
+equals.addEventListener('click', e => {
+    console.log('equals clicked')
+    calculations.forEach(calc => {
+        if (calc.operating === true) {
+            console.log('an operator is true')
+            return calc.calc(nums2Calc)
+        }
+    })
 
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//basic math functions
-function add(a, b) {
-    return a + b
+// basic math functions
+function add(array) {
+    return array.reduce((a, b) => a + b)
 }
 
-function subtract(a, b) {
-    return a - b
+function subtract(array) {
+    return array.reduce((a, b) => a - b)
 }
 
-function multiply(a, b) {
-    return a * b
+function multiply(array) {
+    return array.reduce((a, b) => a * b)
 }
 
-function divide(a, b) {
-    return a / b
+function divide(array) {
+    return array.reduce((a, b) => a / b)
 }
-
-
-
-// function createButtonGrid(num) {
-//     let i = 0;
-//     while (i < num) {
-//         const gridElement = document.createElement('div');
-//         gridElement.classList.add('button');
-//         container.append(gridElement);
-//         i++
-//     }
-// }
-
