db9425f272fccc193478afbd0df33e4be352ea0d
Ian <iankjera@gmail.com>
2021-05-08 17:46:34 -0400

    numbers have max length of 16, using fitty to resize the answer to the display, done untI decide to refactor and/or find bugs

--- app.js
+++ app.js
@@ -5,14 +5,17 @@ const addition = document.querySelector('#add');
 const subtraction = document.querySelector('#subtract');
 const division = document.querySelector('#divide');
 const multiplication = document.querySelector('#multiply');
-const equals = document.querySelector('#equals');
+const equals = document.querySelector('#enter');
 const display = document.querySelector('.display p');
 const answer = document.querySelector('.display h3');
 const clear = document.querySelector('#clear');
 const decimal = document.querySelector('.decimal');
 const negate = document.querySelector('.negate');
-const percentage = document.querySelector('.percentage')
+const percentage = document.querySelector('#percentage')
+const deleteBtn = document.getElementById('delete');
+const allBtns = document.querySelectorAll('.button');
 
+fitty(answer)
 let operationRunning = false;
 let num1 = '';
 let num2 = '';
@@ -57,90 +60,115 @@ function testForOperating() {
 }
 numbers.forEach(number => {
     number.addEventListener('click', function (e) {
-        if (!operationRunning) {
-            if (num1.includes('.') && e.target.innerText.includes('.')) {
-                return
-            } else {
-                console.log(this)
-                answer.innerText = '';
-                operand = '';
-                num1 = e.target.innerText;
-
-                calculations.forEach(calc => calc.operating = false)
-                num2 = '';
-                operationRunning = true;
-                // negateThis(num1)
-                console.log(num1)
-            }
-        }
-        else if (testForOperating()) {
-
-            if (num2.includes('.') && e.target.innerText.includes('.')) {
-                return
-            } else {
-                num2 += e.target.innerText
-            }
-        } else {
-            if (num1.includes('.') && e.target.innerText.includes('.')) {
-                return
-            } else {
-                num1 += e.target.innerText;
-            }
-        }
-        calculation = `${num1} ${operand} ${num2}`
-        display.innerText = calculation
+        numberFunc(this)
     })
 })
 
-operators.forEach(operator => {
-    operator.addEventListener('click', e => {
-        operationRunning = true;
-        if (num1 && num2) {
-            calculations.forEach(calc => {
-                if (calc.operating === true) {
-                    answer.innerText = calc.calc(num1, num2)
-                    num1 = answer.innerText
-                    operand = '';
-                    num2 = '';
-                }
-            })
+function numberFunc(button) {
 
+    if (!operationRunning) {
+        if (num1.includes('.') && button.innerText.includes('.')) {
+            return
+        } else {
+            answer.innerText = '';
+            operand = '';
+            num1 = button.innerText;
+            calculations.forEach(calc => calc.operating = false)
+            num2 = '';
+            operationRunning = true;
         }
+    }
+    else if (testForOperating()) {
+
+        if (num2.includes('.') && button.innerText.includes('.')) {
+            return
+        } else {
+            if (num2.length < 16)
+                num2 += button.innerText
+        }
+    } else {
+        if (num1.includes('.') && button.innerText.includes('.')) {
+            return
+        } else {
+            if (num1.length < 16)
+                num1 += button.innerText;
+        }
+    }
+    calculation = `${num1} ${operand} ${num2}`
+    display.innerText = calculation
+
+}
+operators.forEach(operator => {
+    operator.addEventListener('click', function (e) {
+        operatorFunc(this)
+    })
+})
+
+function operatorFunc(button) {
+    if (!num1) {
+        return
+    }
+    operationRunning = true;
+    if (answer.innerText) {
+        num1 = answer.innerText
+        num2 = '';
+        answer.innerText = ''
+    }
+    if (num1 && num2) {
         calculations.forEach(calc => {
-            if (calc.name === e.target.id) {
-                calc.operating = true
-                operand = calc.operator;
-            } else {
-                calc.operating = false
+            if (calc.operating === true) {
+                answer.innerText = calc.calc(num1, num2)
+                num1 = answer.innerText
+                operand = '';
+                num2 = '';
             }
         })
-        calculation = `${num1} ${operand} ${num2}`
-        display.innerText = calculation
+    }
+    calculations.forEach(calc => {
+        if (calc.name === button.id) {
+            calc.operating = true
+            operand = calc.operator;
+        } else {
+            calc.operating = false
+        }
     })
-})
+    calculation = `${num1} ${operand} ${num2}`
+    display.innerText = calculation
+}
 
 equals.addEventListener('click', e => {
+    equalsFunc();
+})
 
+function equalsFunc() {
     calculations.forEach(calc => {
         if (calc.operating === true && num2) {
             answer.innerText = calc.calc(num1, num2)
             operationRunning = false;
         }
-
     })
+}
+negate.addEventListener('click', e => {
+    if (num1 && !num2) {
+        num1 = reverseNum(num1)
+        calculation = `${num1} ${operand} ${num2}`
+        display.innerText = calculation
+    } else if (num2 && !answer.innerText) {
+        num2 = reverseNum(num2)
+        calculation = `${num1} ${operand} ${num2}`
+        display.innerText = calculation
+    } else {
+        answer.innerText = reverseNum(answer.innerText)
+    }
 })
 
-// function negateThis(number) {
-//     negate.addEventListener('click', e => {
-//         if (parseFloat(number) > 0) {
-//             console.log(this)
-//             number = '-' + number;
-//             num1 = number
-//             calculation = `${num1} ${operand} ${num2}`
-//             display.innerText = calculation
-//         }
-//     })
-// }
+function reverseNum(num) {
+    if (parseFloat(num) > 0) {
+        return num = '-' + num
+    } else {
+        return `${Math.abs(num)}`
+    }
+}
 
 clear.addEventListener('click', e => {
     num1 = '';
@@ -155,6 +183,10 @@ clear.addEventListener('click', e => {
 })
 
 percentage.addEventListener('click', e => {
+    percentageFunc();
+})
+
+function percentageFunc() {
     if (num1 && num2) {
         calcPercent = true;
         num2 /= Math.pow(10, 2);
@@ -162,34 +194,113 @@ percentage.addEventListener('click', e => {
         calculation = `${num1} ${operand}= (${num2} * ${num1}`;
         calculations.forEach(calc => {
             if (calc.operating === true && num2) {
-                answer.innerText = calc.calc(num1, num2, calcPercent)
+                let getResults = calc.calc(num1, num2, calcPercent)
+                console.log(getResults)
+                answer.innerText = getResults.result;
+                num2 = getResults.percentage;
+                calculation = `${num1} ${operand} ${num2}`
+                display.innerText = calculation
                 operationRunning = false;
             }
-
         })
     }
+}
+
+deleteBtn.addEventListener('click', e => {
+    deleteFunc()
 })
+
+function deleteFunc() {
+    if (num1 && !num2) {
+        num1 = num1.slice(0, -1);
+        calculation = `${num1} ${operand} ${num2}`
+        display.innerText = calculation
+    } else {
+        num2 = num2.slice(0, -1);
+        calculation = `${num1} ${operand} ${num2}`
+        display.innerText = calculation
+
+    }
+}
 // basic math functions
 function add(a, b, calcPercent) {
     if (calcPercent) {
-        return parseFloat(num1) + (num2 * num1)
+        let percentAnswer = {};
+        percentAnswer.result = parseFloat(num1) + (num2 * num1);
+        percentAnswer.percentage = num2 * num1;
+        console.log(percentAnswer);
+        return percentAnswer
     } else {
         return parseFloat(a) + parseFloat(b)
     }
 }
 
-function subtract(a, b) {
-    return parseFloat(a) - parseFloat(b)
+function subtract(a, b, calcPercent) {
+    if (calcPercent) {
+        let percentAnswer = {};
+        percentAnswer.result = parseFloat(num1) - (num2 * num1);
+        percentAnswer.percentage = num2 * num1;
+        console.log(percentAnswer);
+        return percentAnswer
+    } else {
+        return parseFloat(a) - parseFloat(b)
+    }
 }
 
-function multiply(a, b) {
-    return parseFloat(a) * parseFloat(b)
+
+function multiply(a, b, calcPercent) {
+    if (calcPercent) {
+        let percentAnswer = {};
+        percentAnswer.result = parseFloat(num1) * (num2 * num1);
+        percentAnswer.percentage = num2 * num1;
+        console.log(percentAnswer);
+        return percentAnswer
+    } else {
+        return parseFloat(a) * parseFloat(b)
+    }
 }
 
-function divide(a, b) {
-    return parseFloat(a) / parseFloat(b)
+
+function divide(a, b, calcPercent) {
+    if (calcPercent) {
+        let percentAnswer = {};
+        percentAnswer.result = parseFloat(num1) / (num2 * num1);
+        percentAnswer.percentage = num2 * num1;
+        console.log(percentAnswer);
+        return percentAnswer
+    } else {
+        return parseFloat(a) / parseFloat(b)
+    }
 }
 
-// function percentage(a, b) {
-//     return
-// }
+document.addEventListener('keydown', e => {
+    console.log(e.key)
+    for (let button of allBtns) {
+        if (Array.from(button.classList).includes('number') && e.key == button.innerText) {
+            console.log('clicked a number')
+            return numberFunc(button)
+        }
+    }
+    for (let button of allBtns) {
+        if (Array.from(button.classList).includes('operator') && e.key == button.innerText) {
+            console.log('clicked an operator')
+            return operatorFunc(button)
+        }
+    }
+    for (let button of allBtns) {
+        if (button.id === 'delete' && e.key.toLowerCase() === button.id) {
+            console.log('clicked delete')
+            return deleteFunc()
+        }
+    }
+    for (let button of allBtns) {
+        if (button.id === 'enter' && e.key.toLowerCase() === button.id) {
+            return equalsFunc()
+        }
+    }
+    for (let button of allBtns) {
+        if (button.innerText === '%' && e.key === button.innerText) {
+            return percentageFunc()
+        }
+    }
+})
